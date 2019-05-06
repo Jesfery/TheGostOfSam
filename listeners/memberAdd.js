59ed12f02cf3221d99f37041b1b5742c7050fa86
Jesfery <sean.shanahan@bigpond.com>
2019-05-06 21:33:37 +1000

    Changes to some initial roles

--- listeners/memberAdd.js
+++ listeners/memberAdd.js
@@ -3,7 +3,8 @@ const utils = require('../utils.js');
 function onGuildMemberAdd(member) {
     let guild = member.guild,
         djRole = guild.roles.find(role =>  role.name === 'DJ'),
-        iglRole = guild.roles.find(role => role.name === 'igl');
+        iglRole = guild.roles.find(role => role.name === 'MEDIER-TEAM'),
+        craigRole = guild.roles.find(role => role.name === 'Craig');
 
     if (djRole) {
         member.roles.add(djRole);    
@@ -11,6 +12,9 @@ function onGuildMemberAdd(member) {
     if (iglRole) {
         member.roles.add(iglRole);
     }
+    if (craigRole) {
+        member.roles.add(craigRole);
+    }    
 }
 
 module.exports = {
